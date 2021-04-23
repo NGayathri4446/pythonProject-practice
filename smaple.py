f12162f69b2f290088c4a023bb4825beceff3ea1
NGayathri4446 <58762705+NGayathri4446@users.noreply.github.com>
2021-04-23 18:55:11 +0530

    practice dict

---
+++ smaple.py
@@ -0,0 +1,19 @@
+my_boolean=True
+print(my_boolean)
+
+square={1:2,2:3,3:"error",4:5}
+square[8]=64
+square[3]=9
+print(square)
+
+prime={1:2,2:3,4:7,7:17}
+print(prime[prime[4]])
+
+nums={
+    1:"one",
+    2:"two",
+    3:"three"
+}
+print(1 in nums)
+print("three" in nums)
+print(4 not in nums)
